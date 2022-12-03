1219f3895dcde906a29aa29d43b50602c75f048a
JohnCru2010 <juancrualf@outlook.com>
2022-12-03 12:05:19 +0100

    dcfvgbhjk

--- PruebaMoludar/Main.java
+++ PruebaMoludar/Main.java
@@ -1,9 +1,10 @@
 package PruebaMoludar;
 
 import PruebaMoludar.app.test.ClassTest;
+import java.util.*;
 
 public class Main {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         ClassTest coche = new ClassTest();
         coche.acelerar();
         coche.acelerar();
