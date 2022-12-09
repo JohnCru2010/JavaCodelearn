030f1bc25829918f4f4034ffc6a727fee1465e1e
JohnCru2010 <juancrualf@outlook.com>
2022-12-09 20:19:34 +0100

    modifing

--- AbstractClass2/Main.java
+++ AbstractClass2/Main.java
@@ -3,13 +3,13 @@ package AbstractClass2;
 public class Main {
     public static void main (String[] args) {
        Warrior w1 = new Warrior ("Code1", "Tanis");
-       Warrior w2 = new Warrior ("Code2", "Aragorn", 10, 20000, 100, 15, 15);
+       Warrior w2 = new Warrior ("Code2", "Aragorn", 10, 100, 20000, 15, 15);
 
        Burglar b1 = new Burglar ("Code3", "Bilbo");
-       Burglar b2 = new Burglar ("Code4", "Tasslehoff", 10, 20000, 100, 15);
+       Burglar b2 = new Burglar ("Code4", "Tasslehoff", 10, 100, 20000, 15);
 
        Wizard wiz1 = new Wizard ("Code5", "Raistlin");
-       Wizard wiz2 = new Wizard ("Code6", "Gandalf", 10, 20000, 100, 15, 100);
+       Wizard wiz2 = new Wizard ("Code6", "Gandalf", 10, 100, 20000, 15, 100);
 
        w1.gainXP(2000);
        w2.gainXP(2000);
