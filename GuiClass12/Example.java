51cb8afd631cc9c7884abe72a24166440a260a91
johncru2010 <juancrualf@outlook.com>
2023-01-08 18:28:11 +0100

    Exercise

---
+++ GuiClass12/Example.java
@@ -0,0 +1,35 @@
+package GuiClass12;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Example extends JFrame {
+    private JButton bt;
+    private JLabel lab;
+
+    public Example () {
+       super ( "Button" );
+       setSize ( 200,100 );
+       setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+       Container cp = getContentPane ();
+       cp.setLayout ( new FlowLayout () );
+       bt = new JButton ( "Press me!" );
+       bt.addActionListener ( new ButtonPressedEvent () );
+       lab = new JLabel ( "" );
+       cp.add ( bt );
+       cp.add ( lab );
+    }
+
+    public static void main ( String args[] ) {
+       Example obj = new Example ();
+       obj.setVisible ( true );
+    }
+
+    private class ButtonPressedEvent implements ActionListener {
+       public void actionPerformed ( ActionEvent e ) {
+          lab.setText("Button pressed");
+       }
+    }
+}
